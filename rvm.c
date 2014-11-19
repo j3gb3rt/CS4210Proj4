dc18e96cbd31510579ff74f504343b39b4391051
j3gb3rt <jegbert.eggy@gmail.com>
2014-11-19 17:25:17 -0500

    The c file actually has content now

---
+++ rvm.c
@@ -0,0 +1,26 @@
+#include "rvm.h"
+
+//Mappings
+rvm_t rvm_init(const char *directory);
+
+void *rvm_map(rvm_t rvm, const char *segname, int size_to_create);
+
+void rvm_unmap(rvm_t rvm, void *segbase);
+
+void rvm_destroy(rvm_t rvm, const char *segname);
+
+	
+//Transactions
+trans_t rvm_begin_trans(rvm_t rvm, int numsegs, void **segbases);
+
+void rvm_about_to_modify(trans_t tid, void *segbase, int offset, int size);
+
+void rvm_commit_trans(trans_t tid);
+
+void rvm_abort_trans(trans_t tid);
+
+
+//Logging
+void rvm_truncate_log(rvm_t rvm);
+
+void rvm_verbose(int enable_flag);
