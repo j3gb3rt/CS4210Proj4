2c473612362b61e2a41e04793fbd92eb62408d2e
chrisenuf <chrisenuf@gmail.com>
2014-11-20 19:21:51 -0500

    made comments on things we should do to implement transactions

--- rvm.c
+++ rvm.c
@@ -11,6 +11,7 @@
 rvm_t rvm_init(const char *directory) {
 	int error;
 
+	//create list with segment transaction mappings
 	error = mkdir(directory, S_IRUSR | S_IWUSR);
 	if (error) {
 		printf("You derped bro: %s\n", strerror(errno));
@@ -25,13 +26,24 @@ void rvm_destroy(rvm_t rvm, const char *segname);
 
 	
 //Transactions
-trans_t rvm_begin_trans(rvm_t rvm, int numsegs, void **segbases);
+trans_t rvm_begin_trans(rvm_t rvm, int numsegs, void **segbases){
+//check through segment/trans mappings then write to them
+}
 
-void rvm_about_to_modify(trans_t tid, void *segbase, int offset, int size);
+void rvm_about_to_modify(trans_t tid, void *segbase, int offset, int size){
+//make sure segment is mapped to transaction
+//save existing segment values in memory in case of abort
+}
 
-void rvm_commit_trans(trans_t tid);
+void rvm_commit_trans(trans_t tid){
+//write committed new values to log
+//erase saved old-values from memory
+}
 
-void rvm_abort_trans(trans_t tid);
+void rvm_abort_trans(trans_t tid){
+//return segment values to the old-values saved in memory
+//erase saved old-values from memory
+}
 
 
 //Logging
